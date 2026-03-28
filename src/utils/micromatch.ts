d141b4c9280e7c06f39053fa45803e387a4a8644
kiki-kanri <a470666@gmail.com>
2026-03-28 18:21:51 +0800

    refactor: update `micromatch` re-export syntax for TypeScript compatibility

--- src/utils/micromatch.ts
+++ src/utils/micromatch.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error Ignore this error.
-import _micromatch from '@kikiutils/micromatch';
+import * as _micromatch from '@kikiutils/micromatch';
 import type * as micromatchType from 'micromatch';
 
-export const micromatch = _micromatch as unknown as typeof micromatchType;
+export const micromatch = _micromatch.default as unknown as typeof micromatchType.default;
